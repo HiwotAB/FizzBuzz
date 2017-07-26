379faf29449d3bbb6d400408f60ea2baae897cb9
student <student@GBTC440J08LX.montgomerycollege.edu>
2017-07-26 17:22:56 -0400

    FizzBuzz

--- src/FizzBuzz/FizzBuzzPrint.java
+++ src/FizzBuzz/FizzBuzzPrint.java
@@ -5,7 +5,7 @@ public class FizzBuzzPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	System.out.println("Print Numbers 1 to 100");// Display the message to the user that what the program work for.  
-	for(int i=1;i<=100;i++){// for loop that is used to intialize value ,check at maximum limits and increment the value by one.
+	for(int i=1;i<=100;i++){// for loop that is used to intializes value ,check at maximum limits and increment the value by one.
 		if(i%3==0 && i%5==0){// check the print number is multiply by 3 and by 5
 			System.out.println("FizzBuzz");//Print the message if it is multiply with both.
 		}
